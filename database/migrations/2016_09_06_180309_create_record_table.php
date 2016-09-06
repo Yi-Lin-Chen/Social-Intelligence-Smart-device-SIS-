4d22fa0b50519b4bb3fb6ec319ba69828dbafb53
Jeremy Yen <jeremy5189@gmail.com>
2016-09-06 18:04:26 +0800

    Add record table

---
+++ database/migrations/2016_09_06_180309_create_record_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecordTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('record', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('access_id');
+            $table->string('note');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('record');
+    }
+}
