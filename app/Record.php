75b7a274ffd99ebf1f79c2c70acace2987693512
Jeremy Yen <jeremy5189@gmail.com>
2016-09-06 18:06:57 +0800

    Add Model

--- app/Record.php
+++ app/Record.php
@@ -2,20 +2,17 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class Record extends Model
 {
-    use Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'phone', 'level'
+        'access_id', 'note',
     ];
 
     /**
@@ -24,6 +21,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+
     ];
 }
